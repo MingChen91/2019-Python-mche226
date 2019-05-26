351b36107369c7b318d1d164a9f93432634ece48
MingChen91 <mingc.mingc@gmail.com>
2019-05-27 00:40:53 +1200

    javascript and database lecture

--- Databases/testdb.py
+++ Databases/testdb.py
@@ -3,6 +3,11 @@ import sqlite3
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
-c.execute("create table user (id integer primary key autoincrement not null, username text not null, password not null, age integer, created_at real not_null, favourite_color text) ")
+# c.execute("create table user (id integer primary key autoincrement not null, username text not null, password not null, age integer, created_at real not_null, favourite_color text) ")
+c.execute("""insert into user
+    (username, password, created_at)
+    values
+    ("hammond","awesome",0)""")
+
 conn.commit()
-conn.close()
+conn.close()  
