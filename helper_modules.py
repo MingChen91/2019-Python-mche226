1c28f1858144ddba95bda34997625ede39a9d8af
MingChen91 <mingc.mingc@gmail.com>
2019-06-02 22:23:27 +1200

    fixed ip, getting it from identme to get external ip

--- helper_modules.py
+++ helper_modules.py
@@ -1,15 +1,22 @@
-import socket
+# import socket
+import urllib.request
+
 def get_ip():
-    """ Returns the local IP"""
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+    """ Returns external IP by getting it from ident.me """
+    # if identme is down, use the commented code. which only works sometimes
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+    return external_ip
+
+    # """ Returns the local IP"""
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # try:
+    #     s.connect(('10.255.255.255', 1))
+    #     IP = s.getsockname()[0]
+    # except:
+    #     IP = '127.0.0.1'
+    # finally:
+    #     s.close()
+    # return IP
 
 
 def get_pubkey(user_list_dict,target_username):
